8122dd2755e1e5617c64a0acdc478ab13d714807
Kevin Cameron <kevinacameron@hotmail.com>
2020-03-01 11:16:41 -0800

    Action production

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 
-const gulp         = require('gulp');
-const less         = require('gulp-less');
+const gulp = require('gulp');
+const less = require('gulp-less');
+const rename = require('gulp-rename');
+const header = require('gulp-header');
+const uglify = require('gulp-uglifycss');
+const noop = require('gulp-noop');
+
+const args = process.argv;
 
 gulp.task('default', function() {
+    const production = args.includes('--production');
+
+    console.log(`prod: ${production}`);
+
     return gulp.src('src/*.less')
         // .pipe(plumber({
         //     errorHandler : function(err) {
@@ -14,6 +24,13 @@ gulp.task('default', function() {
         // }))
         // .pipe(sourcemaps.init())
         .pipe(less())
+        .pipe(rename(path => {
+            if (production) {
+                path.basename += ".min";
+            }
+        }))
+        .pipe(production ? uglify() : noop())
+        .pipe(header(`/* build: ${Date.now()} */\n`))
         // .pipe(autoprefixer({
         //     browsers : [ 'last 3 versions', 'iOS >= 9' ],
         //     cascade  : false
